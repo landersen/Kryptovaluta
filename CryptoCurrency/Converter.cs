5b2db5a3b0bc6202196fef462c63e7784da74cf1
Lasse Maglebjerg <lasse.maglebjerg@ist.com>
2021-10-05 12:52:35 +0200

    Updated CrytoCurrency for the assignments of week 5

--- CryptoCurrency/Converter.cs
+++ CryptoCurrency/Converter.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Collections;
 
 namespace CryptoCurrency
 {
     public class Converter
     {
+        Hashtable currencyList;
+
+        public Converter()
+        {
+            currencyList = new Hashtable();
+            currencyList.Add("BitCoin",10.0); 
+            currencyList.Add("Ripple",20.0);
+            currencyList.Add("DogCoin",30.0);
+        }
+
         /// <summary>
         /// Angiver prisen for en enhed af en kryptovaluta. Prisen angives i dollars.
         /// Hvis der tidligere er angivet en værdi for samme kryptovaluta, 
@@ -13,7 +24,13 @@ namespace CryptoCurrency
         /// <param name="price">Prisen på en enhed af valutaen målt i dollars. Prisen kan ikke være negativ</param>
         public void SetPricePerUnit(String currencyName, double price)
         {
-
+            if (currencyList.Contains(currencyName))
+            {
+                currencyList[currencyName] = price;
+            } else
+            {
+                currencyList.Add(currencyName, price);
+            }
         }
 
         /// <summary>
@@ -27,7 +44,22 @@ namespace CryptoCurrency
         /// <returns>Værdien af beløbet i toCurrencyName</returns>
         public double Convert(String fromCurrencyName, String toCurrencyName, double amount) 
         {
-            return 0;
+            if (currencyList.ContainsKey(fromCurrencyName) && currencyList.ContainsKey(toCurrencyName)) {
+                double fromValue = (double)CurrencyList[fromCurrencyName];
+                double toValue = (double)CurrencyList[toCurrencyName];
+                double fromValueAndAmount = fromValue * amount;
+                double amountOfNewCurrency = fromValueAndAmount / toValue;
+
+                return amountOfNewCurrency;
+            } else
+            {
+                throw new ArgumentException("The currency does not exist");
+            }
+           
         }
+
+        public Hashtable CurrencyList { get => currencyList; set => currencyList = value; }
+
+
     }
 }
